e779accbb21c01021185b039a09a55b83dd4a19f
GuilhermeGuerraCortelini <trabalhodeweb524@gmail.com>
2024-09-05 09:44:38 -0300

    Progresso

---
+++ Jogo/src/Jogo.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+
+
+const Jogo = () => {
+    const [jogo, setJogo] = useState[0]
+
+
+    return (
+        <>
+
+        </>
+    )
+}
+
+export default Jogo
--- Jogo/src/main.jsx
+++ Jogo/src/main.jsx
@@ -1,10 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import Jogo from './Jogo.jsx'
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  // <StrictMode>
+  <>
     <App />
-  </StrictMode>,
+    <Jogo />
+  </>
+  // </StrictMode>
 )
